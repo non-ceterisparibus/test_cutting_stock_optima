82a757752c176adf3ccb3375d77418a8ef3c2dd8
Thu Duong <46911843+non-ceterisparibus@users.noreply.github.com>
2024-05-27 17:33:33 +0700

    Update auxi.py

--- codes/auxi.py
+++ codes/auxi.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import numpy as np
     
+def change_sign_numbers(df):
+    numeric_cols = df.select_dtypes(include=np.number).columns
+    df[numeric_cols] = np.where(df[numeric_cols] < 0, -df[numeric_cols], np.abs(df[numeric_cols]))
+    return df
 
 def stock_excel_to_dict(file_path, key_column, value_columns):
     df = pd.read_excel(file_path)
@@ -21,19 +25,5 @@ def finish_excel_to_dict(file_path, key_column, value_columns):
         fkey = row[key_column]
         values = {column: row[column] for column in value_columns}
         result[f"F{fkey}"] = values
-
     return result
-
-if __name__ == "__main__":
-    # file_path = "data/test_mc_df.xlsx"
-    # stock_column = "Inventory_ID"
-    # value_columns = ["width", "weight"]
-    # stocks = stock_excel_to_dict(file_path, stock_column, value_columns)
-    # print(stocks)
-
-    finish_file_path = "data/test_finish_df.xlsx"
-    finish_column = "Order_ID"
-    finish_value_columns = ["width", "need_cut","fc1"] 
-    finish = finish_excel_to_dict(finish_file_path,finish_column,finish_value_columns)
-    print(finish)
 
