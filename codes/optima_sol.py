54041d1228e3b49c6191df2d24bed5bb7cde6991
Thu Duong <46911843+non-ceterisparibus@users.noreply.github.com>
2024-05-27 17:32:51 +0700

    update process df and solver

--- codes/optima_sol.py
+++ codes/optima_sol.py
@@ -7,8 +7,9 @@ SOLVER_MINLO = "ipopt"
 BOUND = 0.3
 
 from amplpy import AMPL
+from ortools.linear_solver import pywraplp
 
-def cut_patterns_by_stock(width_s, weight_s, finish, patterns, BOUND_KEY, MIN_MARGIN):
+def cut_stock_by_patterns(width_s, weight_s, finish, patterns, BOUND_KEY, MIN_MARGIN):
     m = AMPL()
     m.eval("reset data;")
     m.eval(
@@ -27,7 +28,7 @@ def cut_patterns_by_stock(width_s, weight_s, finish, patterns, BOUND_KEY, MIN_MA
 
         # upper bound with over-cut
         param f_upper_demand{F};
-        param demand_finish{F};
+        # param demand_finish{F};
         # how many f pieces are returned from pattern p
         param a{F, P};
         # which stock s is choosen for pattern p
@@ -58,7 +59,7 @@ def cut_patterns_by_stock(width_s, weight_s, finish, patterns, BOUND_KEY, MIN_MA
     m.param["width_f"] = {f: finish[f]["width"] for f in finish.keys()}
 
     m.param["wu"] = weight_s/width_s  # stock weight per unit (unique)
-    m.param["demand_finish"] = {f: finish[f]["need_cut"] for f in finish.keys()}
+    # m.param["demand_finish"] = {f: finish[f]["need_cut"] for f in finish.keys()}
     m.param["f_upper_demand"] = {f: finish[f][f"upper_bound_{BOUND_KEY}"] for f in finish.keys()}
 
     a = {
@@ -73,6 +74,117 @@ def cut_patterns_by_stock(width_s, weight_s, finish, patterns, BOUND_KEY, MIN_MA
 
     opt_patterns = [p for p in range(len(patterns)) if m.var["b"][p].value() > 0]
     return opt_patterns
+
+def solve_cutting_stock_ampl(width_s, weight_s, finish, naive_patterns, MIN_MARGIN,BOUND_KEY):
+    m = AMPL()
+    m.eval("reset data;")
+    m.eval(
+    """
+        reset;
+        set F;
+
+        # width stock
+        param width_s integer;
+        # width stock minus min_margin
+        param width_s_min_margin integer;
+        # weight per unit of stock
+        param wu > 0;
+        # width finished pieces
+        param width_f{F};
+        # upper bound with over-cut
+        param f_upper_demand{F};
+        param a_upper_bound{F};
+        var a{f in F} integer >= 0, <= a_upper_bound[f];
+        maximize total_width:
+          sum{f in F} a[f] * width_f[f];
+        subject to feasible_pattern_min_margin:
+          sum{f in F} a[f] * width_f[f] <= width_s_min_margin;
+        subject to feasible_pattern_max_margin:
+          sum{f in F} a[f] * width_f[f] >= 0.96 * width_s;
+        subject to weight_demand {f in F}:
+            a[f] * width_f[f] * wu <= f_upper_demand[f];
+    """
+    )
+    m.set["F"] = list(finish.keys())
+    # m.set["P"] = list(range(len(finish)))  # neu range nay ko du lon thi ko tim duoc pattern
+    m.param["width_s"] = width_s
+    m.param["width_s_min_margin"] = width_s - MIN_MARGIN
+    m.param["width_f"] = {f: finish[f]["width"] for f in finish.keys()}
+    m.param["wu"] = weight_s /width_s # stock weight per unit (unique)
+    m.param["f_upper_demand"] = {f: finish[f][f"upper_bound_{BOUND_KEY}"] for f in finish.keys()}
+    a_upper_bound = {f: max([naive_patterns[i]['cuts'][f] for i,_ in enumerate(naive_patterns)]) for f in finish.keys()}
+    m.param["a_upper_bound"] = a_upper_bound
+
+    m.option["solver"] = SOLVER_MILO
+    m.get_output("solve;")
+
+    # opt_patterns = [p for p in range(len(finish)) if m.var["b"][p].value() > 0]
+    opt_patterns = {f: m.var["a"][f].value() for f in finish.keys()}
+    return opt_patterns
+
+def solve_cutting_stock_ortools(finish, width_s, weight_s, MIN_MARGIN, BOUND_KEY, naive_patterns, SOLVER_MILO='SCIP'):
+    """"
+    # upper bound with over-cut
+        param f_upper_demand{F};
+        param a_upper_bound{F};
+        var a{f in F} integer >= 0, <= a_upper_bound[f];
+        maximize total_width:
+          sum{f in F} a[f] * width_f[f];
+        subject to feasible_pattern_min_margin:
+          sum{f in F} a[f] * width_f[f] <= width_s_min_margin;
+        subject to feasible_pattern_max_margin:
+          sum{f in F} a[f] * width_f[f] >= 0.96 * width_s;
+        subject to weight_demand {f in F}:
+            a[f] * width_f[f] * wu <= f_upper_demand[f];
+    """
+    # Create the solver
+    solver = pywraplp.Solver.CreateSolver(SOLVER_MILO)
+    if not solver:
+        print("Solver not found.")
+        return
+
+    # Data and parameters
+    F = list(finish.keys())
+    width_s_min_margin = width_s - MIN_MARGIN
+    width_f = {f: finish[f]["width"] for f in finish.keys()}
+    wu = weight_s / width_s
+    f_upper_demand = {f: finish[f][f"upper_bound_{BOUND_KEY}"] for f in finish.keys()}
+    a_upper_bound = {f: max([naive_patterns[i]['cuts'][f] for i, _ in enumerate(naive_patterns)]) for f in finish.keys()}
+
+    # Decision variables
+    a = {f: solver.IntVar(0, a_upper_bound[f], f'a[{f}]') for f in F}
+
+    # Objective function: maximize total width
+    solver.Maximize(solver.Sum(a[f] * width_f[f] for f in F))
+
+    # Constraints
+    # Feasible pattern min margin
+    solver.Add(solver.Sum(a[f] * width_f[f] for f in F) <= width_s_min_margin)
+
+    # Feasible pattern max margin
+    solver.Add(solver.Sum(a[f] * width_f[f] for f in F) >= 0.96 * width_s)
+
+    # Weight demand
+    for f in F:
+        solver.Add(a[f] * width_f[f] * wu <= f_upper_demand[f])
+
+    # Solve the problem
+    status = solver.Solve()
+
+    # Check the result
+    if status == pywraplp.Solver.OPTIMAL:
+        print("Solution:")
+        for f in F:
+          print(f'[{f}] = {a[f].solution_value()}')
+        trim_loss = width_s - sum([finish[f]["width"]*a[f].solution_value() for f in finish.keys()])
+        weight_loss = trim_loss * wu
+        weight_cut = sum([finish[f]["width"]*a[f].solution_value() for f in finish.keys()]) * wu
+        over_cut = sum([finish[f]["need_cut"] for f in finish.keys()]) + weight_cut
+        # print('Total w =', solver.Objective().Value())
+        # print(f"Trim Loss: {trim_loss}, Weigh loss : {weight_loss}")
+        return {f'trim_loss: {trim_loss},over_cut : {over_cut},weight_loss: {weight_loss}'}
+    else:
+        print("The problem does not have an optimal solution.")
 
 # error and defective 
 def cut_patterns_by_defective_stock(width_s, weight_s, finish, patterns, BOUND_KEY):
--- codes/process_df.py
+++ codes/process_df.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from amplpy import AMPL
+from typing import Dict, Any
 # improve by polars
 
 # Set Obj
@@ -44,7 +45,70 @@ def query_by_params(file_path, params):
     
     return filtered_df
 
-def create_upper_bound_need_cut(df, BOUND_VALUE):
+def check_need_cut_qty(finish_df):
+    # Check if 'column_name' has any values smaller than 0
+    has_need_cut = (finish_df['need_cut'] < -50).any()
+
+    if has_need_cut:
+        total_need_cut = finish_df['need_cut'][finish_df['need_cut'] < -50]
+
+        # Sum the negative values
+        sum_of_total_need_cuts = -total_need_cut.sum()
+        return sum_of_total_need_cuts
+    else:
+        print("There is no need cut")
+
+def choose_stocks_by_need_cuts(df, sum_of_total_need_cuts, over_cut_rate):
+    # Initialize variables
+    cumulative_sum = 0
+    rows_to_include = 0
+
+    # Iterate through the DataFrame rows
+    for value in df['weight']:
+        cumulative_sum += value
+        rows_to_include += 1
+        if cumulative_sum > sum_of_total_need_cuts *(1+over_cut_rate):
+            break
+
+    # Filter the DataFrame to include the first few rows
+    try:
+        filtered_df = df.iloc[:rows_to_include]
+    except IndexError:
+        filtered_df = df
+    return filtered_df
+
+def choose_stock_by_status(df):
+    result = df['status'].isin(['R:REWIND'
+                            ,'Z:SEMI MCOIL'
+                            ,'S:SEMI FINISHED'])
+    if any(result):
+        sorted_df = df[df['status'].isin(['R:REWIND'
+                                        ,'Z:SEMI MCOIL'
+                                        ,'S:SEMI FINISHED']
+                                        )] # need standardization
+    else:
+        sorted_df = df[df['status'].isin(['M:RAW MATERIAL'
+                                                        # ,'Z:SEMI MCOIL'
+                                                        # ,'S:SEMI FINISHED'
+                                                        ])] # need standardization
+    return sorted_df
+
+
+def filter_stock_df_to_dict(df, stock_id, value_columns, sum_of_total_need_cuts):
+
+    # Sort data according to the priority of FIFO
+    sorted_df = df.sort_values(by=['receiving_date','weight'], ascending=[True, False])
+
+    sorted_df = choose_stock_by_status(sorted_df)
+    sorted_df = choose_stocks_by_need_cuts(sorted_df, sum_of_total_need_cuts, over_cut_rate = 0.3)
+    # Set the index of the DataFrame to 'stock_id'
+    sorted_df.set_index(stock_id, inplace=True)
+    # Convert DataFrame to dictionary
+    result = sorted_df[value_columns].to_dict(orient='index')
+
+    return result
+
+def create_upper_bound_need_cut(df, BOUND_VALUE=None):
     """
     Always recalculate with new need cut
     Minus need_cut will always right for DEFAULT & USER SETTING
@@ -52,10 +116,10 @@ def create_upper_bound_need_cut(df, BOUND_VALUE):
     IMPROVE: co the chi can 1 cot upper bound
     """
     # Limited case only be in the consideration if NEED_CUT < 0:
-    df['upper_bound_limited'] = np.where(df['need_cut'] < 0, 0.3 * df['fc1'] - df['need_cut'], np.nan)
     def create_fc_list(x):
         return [f"fc{i}" for i in range(1, x+1)]
-    try:
+    
+    if BOUND_VALUE is not None:
         fc_columns = create_fc_list(BOUND_VALUE)
 
         if BOUND_VALUE <= 3:
@@ -64,40 +128,24 @@ def create_upper_bound_need_cut(df, BOUND_VALUE):
         else:
             # User setting allowing stock after cut < original need cut + X<=6 months forecast
             df['upper_bound_user_setting'] =  df[fc_columns].sum(axis=1) - df['need_cut']
-    except:
-        pass
+
+        df['upper_bound_po'] = df[fc_columns].sum(axis=1)
+    else:
+        df['upper_bound_limited'] = np.where(df['need_cut'] < 0, 0.3 * df['fc1'] - df['need_cut'], np.nan)
+        df['upper_bound_po']  = df['upper_bound_limited'] 
     
     return df
 
-def filter_stock_df_to_dict(df, stock_id, value_columns, PRIORITY):
+def check_finish_weight_per_stock(weight_s: float, width_s: float, finish: Dict[str, Dict[str, Any]], BOUND_KEY: str) -> Dict[str, bool]:
+    wu = weight_s / width_s # MC WEIGHT PER UNIT
+    weight_f = {f: finish[f]["width"]*wu for f in finish.keys()}
+    f_upper_demand = {f: finish[f][f"upper_bound_{BOUND_KEY}"] for f in finish.keys()}
 
-    # Sort data according to the priority of FIFO
-    sorted_df = df.sort_values(by=['receiving_date','weight'], ascending=[True, False])
+    check_f ={f: weight_f[f] < f_upper_demand[f] for f in finish.keys()}
 
-    # CASE 1: TRY TO OPTIMIZE WITH SEMI AND REWIND FIRST
-    if PRIORITY == "CASE_1":
-        sorted_df = sorted_df[sorted_df['status'].isin(['R:REWIND'
-                                                        ,'Z:SEMI MCOIL'
-                                                        ,'S:SEMI FINISHED']
-                                                        )] # need standardization
-    
-    # CASE 2: WITH NORMAL MC HAVING TOTAL WEIGHT >> TOTAL NEED CUT 
-    elif PRIORITY == "CASE_2":
-        sorted_df = sorted_df[sorted_df['status'].isin(['M:RAW MATERIAL'
-                                                        # ,'Z:SEMI MCOIL'
-                                                        # ,'S:SEMI FINISHED'
-                                                        ])] # need standardization
-    else:
-        pass # case cat lai FG or MC defective AND xu ly truong hop empty value
+    return check_f
 
-    # Set the index of the DataFrame to 'stock_id'
-    sorted_df.set_index(stock_id, inplace=True)
-    # Convert DataFrame to dictionary
-    result = sorted_df[value_columns].to_dict(orient='index')
-
-    return result
-
-def filter_finish_excel_to_dict(df, finish_id, value_columns, BOUND_KEY, BOUND_VALUE):
+def filter_finish_df_to_dict(df, finish_id, value_columns, BOUND_VALUE):
     """
     Filter test case with finished goods have need_cut < 0 is the need_cut to be considered
     Note: 
@@ -106,8 +154,8 @@ def filter_finish_excel_to_dict(df, finish_id, value_columns, BOUND_KEY, BOUND_V
     """
     # Create the upper bound for need cut
     filtered_df = create_upper_bound_need_cut(df, BOUND_VALUE)
-    # Filter the DataFrame based on the upper bound column
-    needcut_df = filtered_df[filtered_df['need_cut'] <= filtered_df[f"upper_bound_{BOUND_KEY}"]]
+    # Filter the DataFrame based on the upper bound column (always right even need cut > 0)
+    needcut_df = filtered_df[filtered_df['need_cut'] <= filtered_df["upper_bound_po"]]
 
     # Sort DataFrame by 'need_cut'(negative) column in ascending order, 'width' column in descending order
     sorted_df = needcut_df.sort_values(by=['width','need_cut'], ascending=[False,True])
@@ -119,13 +167,27 @@ def filter_finish_excel_to_dict(df, finish_id, value_columns, BOUND_KEY, BOUND_V
 
     return result
 
-def pandas_to_ampl_obj(df):
-    # 1. Send the data from "amt_df" to AMPL and initialize the indexing set "FOOD"
-    ampl.set_data(food_df, "FOOD")
-    # 2. Send the data from "nutr_df" to AMPL and initialize the indexing set "NUTR"
-    ampl.set_data(nutr_df, "NUTR")
-    # 3. Set the values for the parameter "amt" using "amt_df"
-    ampl.get_parameter("amt").set_values(amt_df)
+def filter_stock_df(df, stock_id, value_columns):
+    status_conditions = df['status'].isin(['R:REWIND'
+                            ,'Z:SEMI MCOIL'
+                            ,'S:SEMI FINISHED'])
+    
+    # Sort data according to the priority of FIFO
+    sorted_df = df.sort_values(by=['receiving_date', 'weight'], ascending=[True, False])
+
+    # Filter dataframe based on status conditions
+    if any(status_conditions):
+        sorted_df = sorted_df[sorted_df['status'].isin(['R:REWIND', 'Z:SEMI MCOIL', 'S:SEMI FINISHED'])]
+    else:
+        sorted_df = sorted_df[sorted_df['status'].isin(['M:RAW MATERIAL'])]
+
+    # Set the index of the DataFrame to 'stock_id'
+    sorted_df.set_index(stock_id, inplace=True)
+    
+    # Select desired columns
+    result = sorted_df[value_columns]
+
+    return result
 
 if __name__ == "__main__":
     # file_path = "../data/test_mc_df.xlsx"
@@ -137,5 +199,5 @@ if __name__ == "__main__":
     finish_file_path = "data/test_finish_df.xlsx"
     finish_key = "order_id"
     finish_value_columns = ["width", "need_cut","fc1"] 
-    finish = filter_finish_excel_to_dict(finish_file_path,finish_key,finish_value_columns)
+    finish = filter_finish_df_to_dict(finish_file_path,finish_key,finish_value_columns)
     print(finish)
