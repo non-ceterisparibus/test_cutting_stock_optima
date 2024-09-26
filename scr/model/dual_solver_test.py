c7256c180f3225d954f4cb35f5b2b65d3b956954
non-ceterisparibus <duongthu1012@gmail.com>
2024-09-26 21:13:41 +0700

    Update dual_solver_test.py

--- scr/model/dual_solver_test.py
+++ scr/model/dual_solver_test.py
@@ -2,16 +2,16 @@ import pandas as pd
 import numpy as np
 import copy
 # from model import FinishObjects, StockObjects
-from pulp import LpMaximize, LpMinimize, LpProblem, LpVariable, lpSum, PULP_CBC_CMD, GLPK ,GLPK_CMD, LpAffineExpression, LpBinary, value
+from pulp import LpMaximize, LpMinimize, LpProblem, LpVariable, lpSum, PULP_CBC_CMD, GLPK ,GLPK_CMD, LpBinary, value
 
 # DEFINE PROBLEM
 class testDualProblem:
-    def __init__(self, dual_finish, dual_stocks):
-        self.len_stocks = len(dual_stocks)
-        self.dual_finish = dual_finish
-        self.dual_stocks = dual_stocks
-        self.start_stocks = dual_stocks
-        self.start_finish = dual_finish
+    def __init__(self, finish, stocks):
+        self.len_stocks = len(stocks)
+        self.dual_finish = finish
+        self.dual_stocks = stocks
+        self.start_stocks = stocks
+        self.start_finish = finish
         self.final_solution_patterns = []
 
     # PHASE 1: Naive/ Dual Pattern Generation
@@ -66,7 +66,7 @@ class testDualProblem:
         self.dual_finish = {k: v for k, v in dump_ls.items() if v['upper_demand_line'] > 0} # xem lai dieu kien nay, tuc la neu cat dai nay voi stock hien co thì overcut lon
     
     # PHASE 2: Pattern Duality
-    def _filter_out_overlap_stock(self):
+    def _filter_out_overlapped_stock(self):
         """
         Find stocks {stock:receiving_date,width, weight, qty} 
         with condition, take the list of pattern diff from the key
@@ -188,7 +188,7 @@ class testDualProblem:
         remove_stock = True
         self.max_key = None
         while remove_stock == True:
-            self._filter_out_overlap_stock()
+            self._filter_out_overlapped_stock()
             new_pattern = self._generate_dual_pattern() 
             dual_pat = []
             while (new_pattern not in dual_pat) and (new_pattern is not None):
@@ -271,11 +271,11 @@ class testDualProblem:
 
         # PARAMETER - unit weight of stock
         c = {p: self.chosen_stocks[pattern["stock"]]["weight"]/self.chosen_stocks[pattern["stock"]]["width"] for p, pattern in enumerate(self.filtered_patterns)}
-        s = {p: self.chosen_stocks[pattern["stock"]]["weight"] for p, pattern in enumerate(self.filtered_patterns)}
-        sp = {(s, p): 1 if self.filtered_patterns[p]["stock"]== s else 0 for p in P for s in S}
+        # s = {p: self.chosen_stocks[pattern["stock"]]["weight"] for p, pattern in enumerate(self.filtered_patterns)}
+        # sp = {(s, p): 1 if self.filtered_patterns[p]["stock"]== s else 0 for p in P for s in S}
         
         # Define VARIABLES
-        x = {p: LpVariable(f"X_{p}",lowBound=0, upBound=1,cat='Integer') for p in P}
+        x = {p: LpVariable(f"X_{p}",lowBound=0,upBound=1,cat='Integer') for p in P}
         
         # Create a LP minimization problem
         prob = LpProblem("PatternCuttingProblem", LpMinimize)
@@ -291,6 +291,60 @@ class testDualProblem:
         for f in self.dual_finish:
             prob += (
                 lpSum(self.filtered_patterns[p]['cuts'][f] * self.dual_finish[f]['width'] * c[p] * x[p]
+                          for p in range(len(self.filtered_patterns))) >= self.dual_finish[f]['need_cut'] - 0.019*self.dual_finish[f]['average FC'], 
+                f"DemandWeight{f}"
+            )
+            prob += (
+                lpSum(self.filtered_patterns[p]['cuts'][f] * self.dual_finish[f]['width'] * c[p] * x[p]
+                          for p in range(len(self.filtered_patterns))) <= self.dual_finish[f]['upper_bound'], 
+                f"UpperDemandWeight{f}"
+            )
+        
+        # Solve the problem GLPL - IBM solver
+        prob.solve(GLPK(msg=False,path="/opt/homebrew/bin/glpsol"))
+
+        try: # Extract results
+            solution = [
+                        round(x[p].varValue) 
+                        for p in range(len(self.filtered_patterns))
+                    ]
+            # print(solution)
+            
+            self.solution_list = []
+            for i, pattern_info in enumerate(self.filtered_patterns):
+                count = solution[i]
+                if count > 0:
+                    self.solution_list.append({"count": count, **pattern_info})
+                    
+            # self.probstt = "Solved"
+            if prob.status == 1:
+                self.probstt = "Solved"
+            else: self.probstt = "Infeasible"
+        except KeyError: self.probstt = "Infeasible" # khong co nghiem
+        
+    def cbc_optimize_cut(self):
+        # Sets
+        S = list(self.chosen_stocks.keys())
+        P = list(range(len(self.filtered_patterns)))
+
+        # PARAMETER - unit weight of stock
+        c = {p: self.chosen_stocks[pattern["stock"]]["weight"]/self.chosen_stocks[pattern["stock"]]["width"] for p, pattern in enumerate(self.filtered_patterns)}
+        # s = {p: self.chosen_stocks[pattern["stock"]]["weight"] for p, pattern in enumerate(self.filtered_patterns)}
+        # sp = {(s, p): 1 if self.filtered_patterns[p]["stock"]== s else 0 for p in P for s in S}
+        
+        # Define VARIABLES
+        x = {p: LpVariable(f"X_{p}",lowBound=0, upBound=1, cat='Integer') for p in P}
+        
+        # Create a LP minimization problem
+        prob = LpProblem("PatternCuttingProblem", LpMinimize)
+        
+        # Objective function: MINIMIZE total stock use
+        prob += lpSum(x[p] for p in P), "TotalStockUse"
+
+        # Constraints: meet demand for each finished part
+        for f in self.dual_finish:
+            prob += (
+                lpSum(self.filtered_patterns[p]['cuts'][f] * self.dual_finish[f]['width'] * c[p] * x[p]
                           for p in range(len(self.filtered_patterns))) >= self.dual_finish[f]['need_cut'], 
                 f"DemandWeight{f}"
             )
@@ -300,26 +354,22 @@ class testDualProblem:
                 f"UpperDemandWeight{f}"
             )
         
-        # Solve the problem
-        prob.solve(GLPK(msg=False,path="/opt/homebrew/bin/glpsol"))
-        # if prob.status != 1:
-        #     prob.solve(PULP_CBC_CMD(msg=False, options=['--solver', 'highs']))
+        # Solve the problem GLPL - IBM solver
+        print("solver CBC")
+        prob.solve(PULP_CBC_CMD(msg=False, options=['--solver', 'highs']))
         
         try: # Extract results
             solution = [
                         1 if (x[p].varValue > 0 and round(x[p].varValue) == 0) 
                         else round(x[p].varValue) 
                         for p in range(len(self.filtered_patterns))
-                    ]
+                        ]
             self.solution_list = []
             for i, pattern_info in enumerate(self.filtered_patterns):
                 count = solution[i]
                 if count > 0:
                     self.solution_list.append({"count": count, **pattern_info})
-            # self.probstt = "Solved"
-            if prob.status == 1:
-                self.probstt = "Solved"
-            else: self.probstt = "Infeasible"
+            self.probstt = "Solved"
         except KeyError: self.probstt = "Infeasible" # khong co nghiem
     
     def _expand_solution_list(self):
@@ -362,7 +412,7 @@ class testDualProblem:
                 take_stock = current_stock
                 self.final_solution_patterns.append(solution_pattern)
                 
-    def run(self):
+    def run(self, solver):
         #Phase 1
         self.create_finish_demand_by_line_w_naive_pattern()
         
@@ -373,154 +423,158 @@ class testDualProblem:
         self.filter_patterns_and_stocks_by_constr()
         
         #Phase 4
-        self.optimize_cut()
+        if solver == "CBC":
+            self.cbc_optimize_cut()
+        else:
+            self.optimize_cut()
+        
         if self.probstt == 'Solved':
             self.find_final_solution_patterns()
             
-#HELPER
-import math
+# #HELPER
+# import math
 
-def calculate_upper_bounds(finish, bound):
-    mean_3fc = {
-            f: (
-                sum(v for v in (f_info['fc1'], f_info['fc2'], f_info['fc3']) if not math.isnan(v)) /
-                sum(1 for v in (f_info['fc1'], f_info['fc2'], f_info['fc3']) if not math.isnan(v))
-            ) if any(not math.isnan(v) for v in (f_info['fc1'], f_info['fc2'], f_info['fc3'])) else float('nan')
-            for f, f_info in finish.items()
-        }
-    finish = {f: {**f_info, "mean_3fc": mean_3fc[f] if mean_3fc[f]>0 else -f_info['need_cut'] } for f, f_info in finish.items()}
+# def calculate_upper_bounds(finish, bound):
+#     mean_3fc = {
+#             f: (
+#                 sum(v for v in (f_info['fc1'], f_info['fc2'], f_info['fc3']) if not math.isnan(v)) /
+#                 sum(1 for v in (f_info['fc1'], f_info['fc2'], f_info['fc3']) if not math.isnan(v))
+#             ) if any(not math.isnan(v) for v in (f_info['fc1'], f_info['fc2'], f_info['fc3'])) else float('nan')
+#             for f, f_info in finish.items()
+#         }
+#     finish = {f: {**f_info, "mean_3fc": mean_3fc[f] if mean_3fc[f]>0 else -f_info['need_cut'] } for f, f_info in finish.items()}
         
-    # Need_cut van la so am
-    finish = {f: {**f_info, "upper_bound": -f_info['need_cut'] + f_info['mean_3fc']* bound} for f, f_info in finish.items()}
-    return finish      
+#     # Need_cut van la so am
+#     finish = {f: {**f_info, "upper_bound": -f_info['need_cut'] + f_info['mean_3fc']* bound} for f, f_info in finish.items()}
+#     return finish      
 
-if __name__ == "__main__":
-    finish = {
-                  "F626": {
-                     "customer_name": "TEC",
-                     "width": 121.2,
-                     "need_cut": -7005.70698757764,
-                     "fc1": 16388.348,
-                     "fc2": 0.0,
-                     "fc3": 9008.282,
-                     "average FC": 8465.543333333333,
-                     "1st Priority": "NQS",
-                     "2nd Priority": "NST",
-                     "3rd Priority": "HSC",
-                     "Min_weight": 300,
-                     "Max_weight": 500
-                  },
-                  "F636": {
-                     "customer_name": "TEC",
-                     "width": 196.0,
-                     "need_cut": -5663.586956521739,
-                     "fc1": 11943.8700096,
-                     "fc2": 174.68986079999996,
-                     "fc3": 6921.4064136,
-                     "average FC": 6346.655427999999,
-                     "1st Priority": "NQS",
-                     "2nd Priority": "NST",
-                     "3rd Priority": "HSC",
-                     "Min_weight": 150,
-                     "Max_weight": 350
-                  },
-                  "F637": {
-                     "customer_name": "TEC",
-                     "width": 206.0,
-                     "need_cut": -5203.64751552795,
-                     "fc1": 10608.94386,
-                     "fc2": 0.0,
-                     "fc3": 6658.549940000001,
-                     "average FC": 5755.831266666667,
-                     "1st Priority": "NQS",
-                     "2nd Priority": "NST",
-                     "3rd Priority": "HSC",
-                     "Min_weight": 300,
-                     "Max_weight": 500
-                  },
-                  "F624": {
-                     "customer_name": "TEC",
-                     "width": 110.0,
-                     "need_cut": -1280.4992236024846,
-                     "fc1": 1741.616,
-                     "fc2": 1048.29242,
-                     "fc3": 2165.562,
-                     "average FC": 1651.8234733333331,
-                     "1st Priority": "NQS",
-                     "2nd Priority": "NST",
-                     "3rd Priority": "HSC",
-                     "Min_weight": 150,
-                     "Max_weight": 350
-                  },
-                  "F628": {
-                     "customer_name": "TEC",
-                     "width": 135.0,
-                     "need_cut": -203.65877329192546,
-                     "fc1": 2252.57308,
-                     "fc2": 1912.9525500000002,
-                     "fc3": 1331.6751600000002,
-                     "average FC": 1832.4002633333337,
-                     "1st Priority": "NQS",
-                     "2nd Priority": "NST",
-                     "3rd Priority": "HSC",
-                     "Min_weight": 150,
-                     "Max_weight": 350
-                  },
-                  "F646": {
-                     "customer_name": "TEC",
-                     "width": 63.0,
-                     "need_cut": -4.0,
-                     "fc1": 390.9795,
-                     "fc2": 343.1219,
-                     "fc3": 588.3921,
-                     "average FC": 440.83116666666666,
-                     "1st Priority": "NQS",
-                     "2nd Priority": "NST",
-                     "3rd Priority": "HSC",
-                     "Min_weight": 100,
-                     "Max_weight": 300
-                  }
-               }
-    stocks = {
-            "HTV0348/24": {
-               "receiving_date": 44945,
-               "width": 1219,
-               "weight": 7809,
-               "warehouse": "NQS",
-               "status": "M:RAW MATERIAL",
-               "remark": "",
-               "min_margin":5
-            },
-            "HTV0349/24": {
-               "receiving_date": 44945,
-               "width": 1219,
-               "weight": 7904,
-               "warehouse": "NQS",
-               "status": "M:RAW MATERIAL",
-               "remark": "",
-               "min_margin":5
-            },
-            "HTV0340/24": {
-               "receiving_date": 44945,
-               "width": 1219,
-               "weight": 8363,
-               "warehouse": "NQS",
-               "status": "M:RAW MATERIAL",
-               "remark": "",
-               "min_margin":5
-            },
-            "HTV0341/24": {
-               "receiving_date": 44945,
-               "width": 1219,
-               "weight": 8948,
-               "warehouse": "NQS",
-               "status": "M:RAW MATERIAL",
-               "remark": "",
-               "min_margin":5
-            }
-         }
+# if __name__ == "__main__":
+#     finish = {
+#                   "F626": {
+#                      "customer_name": "TEC",
+#                      "width": 121.2,
+#                      "need_cut": -7005.70698757764,
+#                      "fc1": 16388.348,
+#                      "fc2": 0.0,
+#                      "fc3": 9008.282,
+#                      "average FC": 8465.543333333333,
+#                      "1st Priority": "NQS",
+#                      "2nd Priority": "NST",
+#                      "3rd Priority": "HSC",
+#                      "Min_weight": 300,
+#                      "Max_weight": 500
+#                   },
+#                   "F636": {
+#                      "customer_name": "TEC",
+#                      "width": 196.0,
+#                      "need_cut": -5663.586956521739,
+#                      "fc1": 11943.8700096,
+#                      "fc2": 174.68986079999996,
+#                      "fc3": 6921.4064136,
+#                      "average FC": 6346.655427999999,
+#                      "1st Priority": "NQS",
+#                      "2nd Priority": "NST",
+#                      "3rd Priority": "HSC",
+#                      "Min_weight": 150,
+#                      "Max_weight": 350
+#                   },
+#                   "F637": {
+#                      "customer_name": "TEC",
+#                      "width": 206.0,
+#                      "need_cut": -5203.64751552795,
+#                      "fc1": 10608.94386,
+#                      "fc2": 0.0,
+#                      "fc3": 6658.549940000001,
+#                      "average FC": 5755.831266666667,
+#                      "1st Priority": "NQS",
+#                      "2nd Priority": "NST",
+#                      "3rd Priority": "HSC",
+#                      "Min_weight": 300,
+#                      "Max_weight": 500
+#                   },
+#                   "F624": {
+#                      "customer_name": "TEC",
+#                      "width": 110.0,
+#                      "need_cut": -1280.4992236024846,
+#                      "fc1": 1741.616,
+#                      "fc2": 1048.29242,
+#                      "fc3": 2165.562,
+#                      "average FC": 1651.8234733333331,
+#                      "1st Priority": "NQS",
+#                      "2nd Priority": "NST",
+#                      "3rd Priority": "HSC",
+#                      "Min_weight": 150,
+#                      "Max_weight": 350
+#                   },
+#                   "F628": {
+#                      "customer_name": "TEC",
+#                      "width": 135.0,
+#                      "need_cut": -203.65877329192546,
+#                      "fc1": 2252.57308,
+#                      "fc2": 1912.9525500000002,
+#                      "fc3": 1331.6751600000002,
+#                      "average FC": 1832.4002633333337,
+#                      "1st Priority": "NQS",
+#                      "2nd Priority": "NST",
+#                      "3rd Priority": "HSC",
+#                      "Min_weight": 150,
+#                      "Max_weight": 350
+#                   },
+#                   "F646": {
+#                      "customer_name": "TEC",
+#                      "width": 63.0,
+#                      "need_cut": -4.0,
+#                      "fc1": 390.9795,
+#                      "fc2": 343.1219,
+#                      "fc3": 588.3921,
+#                      "average FC": 440.83116666666666,
+#                      "1st Priority": "NQS",
+#                      "2nd Priority": "NST",
+#                      "3rd Priority": "HSC",
+#                      "Min_weight": 100,
+#                      "Max_weight": 300
+#                   }
+#                }
+#     stocks = {
+#             "HTV0348/24": {
+#                "receiving_date": 44945,
+#                "width": 1219,
+#                "weight": 7809,
+#                "warehouse": "NQS",
+#                "status": "M:RAW MATERIAL",
+#                "remark": "",
+#                "min_margin":5
+#             },
+#             "HTV0349/24": {
+#                "receiving_date": 44945,
+#                "width": 1219,
+#                "weight": 7904,
+#                "warehouse": "NQS",
+#                "status": "M:RAW MATERIAL",
+#                "remark": "",
+#                "min_margin":5
+#             },
+#             "HTV0340/24": {
+#                "receiving_date": 44945,
+#                "width": 1219,
+#                "weight": 8363,
+#                "warehouse": "NQS",
+#                "status": "M:RAW MATERIAL",
+#                "remark": "",
+#                "min_margin":5
+#             },
+#             "HTV0341/24": {
+#                "receiving_date": 44945,
+#                "width": 1219,
+#                "weight": 8948,
+#                "warehouse": "NQS",
+#                "status": "M:RAW MATERIAL",
+#                "remark": "",
+#                "min_margin":5
+#             }
+#          }
     
-    finish = calculate_upper_bounds(finish, 1.0)
-    prob = testDualProblem(finish, stocks)
-    prob.run()
+#     finish = calculate_upper_bounds(finish, 1.0)
+#     prob = testDualProblem(finish, stocks)
+#     prob.run()
     
