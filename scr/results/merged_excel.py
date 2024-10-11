b3d941e96ba54ffb866a9dcede243c82ae1320ce
non-ceterisparibus <duongthu1012@gmail.com>
2024-10-11 09:59:10 +0700

    Update merged_excel.py

--- scr/results/merged_excel.py
+++ scr/results/merged_excel.py
@@ -5,7 +5,7 @@ import os
 folder_path = 'scr/results'
 
 # Get a list of all Excel files in the folder
-excel_files = [f for f in os.listdir(folder_path) if f.endswith('.xlsx') and f.startswith('split-cust')]
+excel_files = [f for f in os.listdir(folder_path) if f.endswith('.xlsx') and f.startswith('result-weight-CBC')]
 
 # Initialize an empty list to hold dataframes
 df_list = []
@@ -20,4 +20,4 @@ for file in excel_files:
 merged_df = pd.concat(df_list, ignore_index=True)
 
 # Save the merged dataframe to a new Excel file
-merged_df.to_excel(os.path.join(folder_path, '20240912_test_result_batch1_s_split.xlsx'), index=False)
+merged_df.to_excel(os.path.join(folder_path, '0910_test_result_batch3_added.xlsx'), index=False)
